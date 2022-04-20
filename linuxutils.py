d68605adb09021d6a8e54034a6ee2cbdde680e6f
George Flanagin <gflanagi@richmond.edu>
2022-04-20 10:37:24 -0400

    Added bytes2human to aid with psutils work.

--- linuxutils.py
+++ linuxutils.py
@@ -91,6 +91,22 @@ def byte_scale(i:int, key:str='X') -> str:
             # How did this happen?
             return f"Error: byte_scale({i}, {k})"
 
+def bytes2human(n:int) -> str:
+    # http://code.activestate.com/recipes/578019
+    # >>> bytes2human(10000)
+    # '9.8K'
+    # >>> bytes2human(100001221)
+    # '95.4M'
+    symbols = ('K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y')
+    prefix = {}
+    for i, s in enumerate(symbols):
+        prefix[s] = 1 << (i + 1) * 10
+    for s in reversed(symbols):
+        if n >= prefix[s]:
+            value = float(n) / prefix[s]
+            return '%.1f%s' % (value, s)
+    return "%sB" % n
+
 
 def byte_size(s:str) -> int:
     """
