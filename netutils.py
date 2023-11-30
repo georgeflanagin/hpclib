546c32d4da969ebc2b059657e3a47f64504eada7
George <me@georgeflanagin.com>
2023-11-30 16:11:41 -0500

    Added ability to follow Include directives.

--- netutils.py
+++ netutils.py
@@ -4,11 +4,12 @@
 import typing
 from   typing import *
 
+import tempfile
+
 import paramiko
 
 import fileutils
 from   sloppytree import SloppyTree
-
 
 def get_ssh_host_info(host_name:str=None, config_file:str=None) -> List[Dict]:
     """ Utility function to get all the ssh config info, or just that
@@ -21,15 +22,38 @@ def get_ssh_host_info(host_name:str=None, config_file:str=None) -> List[Dict]:
     """
 
     if config_file is None:
-        config_file = fileutils.expandall("~") + "/.ssh/config"
+        config_file = fileutils.expandall("~/.ssh/config")
+
+    ###
+    # Paramiko does not process Include directives. Let's create a tempfile
+    # from the above, resolve any Include directives, and combine it all into 
+    # one file.
+    ###
+    config_file_lines = open(config_file).readlines()
+    the_name = ""
+    with tempfile.NamedTemporaryFile(mode='w+t', delete=False) as tf:
+        the_name = tf.name
+        for line in config_file_lines:
+            if line.startswith("Include"):
+                fname = fileutils.expandall(line.split()[1])
+                if not fname: continue
+                included_lines = open(fname).readlines()
+                for inc_line in included_lines: 
+                    tf.write(inc_line + "\n")
+            else:
+                tf.write(line + "\n")
+        tf.close()
 
     ssh_conf = paramiko.SSHConfig()
-    ssh_conf.parse(open(config_file))
+    ssh_conf.parse(open(the_name))
 
-    if not host_name: return ssh_conf
-    if host_name == 'all': return ssh_conf.get_hostnames()
-    return ( None if host_name not in ssh_conf.get_hostnames() 
-        else SloppyTree(ssh_conf.lookup(host_name)) )
+    if not host_name: 
+        return ssh_conf
+    elif host_name == 'all': 
+        return SloppyTree({name:ssh_conf.lookup(name) for name in ssh_conf.get_hostnames()})
+    else:
+        return ( None if host_name not in ssh_conf.get_hostnames() 
+            else SloppyTree(ssh_conf.lookup(host_name)) )
 
 
 
