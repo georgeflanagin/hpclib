afc3d19d2bdf1fdec9c89cdd5136712caedfc4ce
Skyler He <skyler.he@richmond.edu>
2024-08-22 18:35:01 -0400

    ordered

--- sloppytree.py
+++ sloppytree.py
@@ -55,15 +55,28 @@ __email__ = 'gflanagin@richmond.edu, yingxinskyler.he@gmail.com'
 __status__ = 'in progress'
 __license__ = 'MIT'
 
+###
+# SloppyException
+###
+class SloppyException(LookupError):
+    def __init__(self, message, original_exception=None):
+        super().__init__(message)
+        self.original_exception = original_exception
+
+    def raise_original(self):
+        """Re-raise the orignal exception if it exists"""
+        if self.original_exception:
+            raise self.original_exception
+        else:
+            raise self 
+
+
+
+
 class SloppyDict: pass
-
-def sloppy(o:object) -> SloppyDict:
-    """
-    Returns a dictionary.
-    """
-    return o if isinstance(o, SloppyDict) else SloppyDict(o)
-
-
+###
+#Utility functions
+###
 def deepsloppy(o:dict) -> Union[SloppyDict, object]:
     """
     Multi level slop.
@@ -82,10 +95,29 @@ def deepsloppy(o:dict) -> Union[SloppyDict, object]:
 
     return o
 
+def sloppy(o:object) -> SloppyDict:
+    """
+    Returns a dictionary.
+    """
+    return o if isinstance(o, SloppyDict) else SloppyDict(o)
+
+###
+# SloppyDict
+###
 class SloppyDict(dict):
     """
     Make a dict into an object for notational convenience.
     """
+    ###
+    # Magic methods
+    ###
+    def __delattr__(self, k:str) -> None:
+        """
+        Deletes the key in the dictionary.
+        """
+        if k in self: del self[k]
+        else: raise SloppyException(f"No element named {k}")
+    
     def __getattr__(self, k:str) -> object:
         """
         Gets the value of the key in the dictionary.
@@ -100,13 +132,9 @@ class SloppyDict(dict):
         self[k] = v
 
 
-    def __delattr__(self, k:str) -> None:
-        """
-        Deletes the key in the dictionary.
-        """
-        if k in self: del self[k]
-        else: raise SloppyException(f"No element named {k}")
-
+    ###
+    # Regular methods
+    ###
     def reorder(self, some_keys:list=[], self_assign:bool=True) -> SloppyDict:
         """
         Sorts the keys in the dictionary.
@@ -130,19 +158,143 @@ class SloppyDict(dict):
         else:
             return new_data
 
-
+    
 
 class SloppyTree: pass
 class SloppyTree(dict):
     """
     Like SloppyDict() only worse -- much worse.
     """
+    ###
+    # Magic methods
+    ###
+    ###
+    # A
+    ###
+    ###
+    # B
+    ###
+    def __bool__(self) -> bool:
+        """
+        When a SloppyTree is evaluated with if, it becomes a boolean.
+        Let's avoid any use of the tree's iterators, and just answer
+        the question, "Is this tree empty?"
+        """
+        return not not len(self.items())
+
+
+    ###
+    # C
+    ###
+    def __call__(self, key_as_str:str) -> object:
+        """
+        Allow for retrieval of a nested key by a string
+        that represents its name. Essentially this:
+
+        t("a.b.c") means t[a][b][c]
+        """
+        ptr = self
+        for k in key_as_str.split('.'):
+            if k not in ptr:
+                raise SloppyException(f"{k=} not found in sub-tree {ptr=}")
+            ptr = v = ptr[k]
+
+    ###
+    # D
+    ###
+    def __delattr__(self, k:str) -> None:
+        """
+        Remove it if we can.
+        """
+        if k in self: del self[k]
+
+    ###
+    # E,F
+    ###
+    ###
+    # G
+    ###
     def __getattr__(self, k:str) -> object:
         """
         Retrieve the element, or implicity call the over-ridden
         __missing__ method, and make a new one.
         """
         return self[k]
+
+
+    def __getstate__(self): return self.__dict__
+
+
+    ###
+    # H,I,J,K
+    ###
+
+    ###
+    # L
+    ###
+    def __len__(self) -> int:
+
+        """
+        return the number of nodes/branches.
+        """
+        return sum(1 for _ in (i for i in self.traverse(False)))
+
+    ###
+    # M
+    ###
+    def __missing__(self, k:str) -> object:
+        """
+        If we reference an element that doesn't exist, we create it,
+        and assign a SloppyTree to its value.
+        """
+        self[k] = SloppyTree()
+        return self[k]
+
+    ###
+    # N,O,P,Q
+    ###
+    ###
+    # I
+    ###
+    def __invert__(self) -> int:
+        """
+        return the number of paths from the root node to the leaves,
+        ignoring the nodes along the way.
+        """
+        return sum(1 for _ in (i for i in self.leaves()))
+
+
+    def __iter__(self) -> object:
+        """
+        NOTE: dict.__iter__ only sees keys, but SloppyTree.__iter__
+        also sees the leaves.
+        """
+        return self.traverse()
+    
+    ###
+    # S 
+    ###
+    def __setattr__(self, k:str, v:object) -> None:
+        """
+        Sets the value to the key, or iterated key. This syntax:
+
+            d[(1, 'c', 6)] = 'value'
+
+        is the same as:
+
+            d[1]['c'][6] = 'value'
+        """
+        # Typical case, k is the key we want.
+        if isinstance(k, str):
+            self[k] = v
+
+        elif len(k) == 1:
+            self[k[0]] = v
+
+        else:
+            for element in k:
+                d = self[element]
+                d[k[1:]] = v
 
 
     def __setitem__(self, k:str, v:object) -> None:
@@ -172,100 +324,41 @@ class SloppyTree(dict):
             sys.exit(1)
 
 
-    def __call__(self, key_as_str:str) -> object:
-        """
-        Allow for retrieval of a nested key by a string
-        that represents its name. Essentially this:
-
-        t("a.b.c") means t[a][b][c]
-        """
-        ptr = self
-        for k in key_as_str.split('.'):
-            if k not in ptr:
-                raise SloppyException(f"{k=} not found in sub-tree {ptr=}")
-            ptr = v = ptr[k]
-        return v
-
-    def __setattr__(self, k:str, v:object) -> None:
-        """
-        Sets the value to the key, or iterated key. This syntax:
-
-            d[(1, 'c', 6)] = 'value'
-
-        is the same as:
-
-            d[1]['c'][6] = 'value'
-        """
-        # Typical case, k is the key we want.
-        if isinstance(k, str):
-            self[k] = v
-
-        elif len(k) == 1:
-            self[k[0]] = v
-
-        else:
-            for element in k:
-                d = self[element]
-                d[k[1:]] = v
-    def __delattr__(self, k:str) -> None:
-        """
-        Remove it if we can.
-        """
-        if k in self: del self[k]
-        #else: raise SloppyException(f"No element named {k}")
-
-    def __invert__(self) -> int:
-        """
-        return the number of paths from the root node to the leaves,
-        ignoring the nodes along the way.
-        """
-        return sum(1 for _ in (i for i in self.leaves()))
-
-
-    def __iter__(self) -> object:
-        """
-        NOTE: dict.__iter__ only sees keys, but SloppyTree.__iter__
-        also sees the leaves.
-        """
-        return self.traverse()
+    def __setstate__(self, d): self.__dict__.update(d)
     
-    def __bool__(self) -> bool:
-        """
-        When a SloppyTree is evaluated with if, it becomes a boolean.
-        Let's avoid any use of the tree's iterators, and just answer
-        the question, "Is this tree empty?"
-        """
-        return not not len(self.items())
-
-
-    def __len__(self) -> int:
-        """
-        return the number of nodes/branches.
-        """
-        return sum(1 for _ in (i for i in self.traverse(False)))
-
-
-    def __missing__(self, k:str) -> object:
-        """
-        If we reference an element that doesn't exist, we create it,
-        and assign a SloppyTree to its value.
-        """
-        self[k] = SloppyTree()
-        return self[k]
-
-
     def __str__(self) -> str:
         return self.printable
+
+
         
 
     ###
-    # All objects derived from dict need these functions if they
-    # are to be pickled or otherwise examined internally.
+    # Regular methods 
     ###
-    def __getstate__(self): return self.__dict__
 
-    def __setstate__(self, d): self.__dict__.update(d)
+    ###
+    ###
+    # A
+    ###
+    def as_tuples(self) -> tuple:
+        """
+        A generator to return all paths from root to leaves as
+        tuples of the nodes along the way.
+        """
+        tup = []
+        for node, indicator in self.traverse():
+            tup.append(node)
+            if not indicator:
+                yield tuple(tup)
+                tup = []
+    ###
+    # B,C,D,E,F,G,H,I,J,K
+    ###
 
+
+    ###
+    # L
+    ###
     def leaves(self) -> object:
         """
         Walk the leaves only, left to right.
@@ -277,14 +370,25 @@ class SloppyTree(dict):
                 yield from SloppyTree(v).leaves()
             else:
                 yield v
-    @property
+    
+    ###
+    # M,N,O
+    ###
+    ###
+    # P
+    ###
     def printable(self) -> str:
         """
         Printing one of these things requires a bit of finesse.
         """
         return pprint.pformat(dict(self), compact=True, sort_dicts=True, indent=4, width=100)
 
-
+    ###
+    # Q,I
+    ###
+    ###
+    # S
+    ###
     def traverse(self, with_indicator:bool=True) -> Union[Tuple[object, int], object]:
         """
         Emit all the nodes of a tree left-to-right and top-to-bottom.
@@ -306,18 +410,6 @@ class SloppyTree(dict):
             else:
                 yield v, 0 if with_indicator else v
 
-    def as_tuples(self) -> tuple:
-        """
-        A generator to return all paths from root to leaves as
-        tuples of the nodes along the way.
-        """
-        tup = []
-        for node, indicator in self.traverse():
-            tup.append(node)
-            if not indicator:
-                yield tuple(tup)
-                tup = []
-
 
     def tree_as_table(self, nested_dict:SloppyTree=None, prepath=()):
         """
@@ -338,14 +430,3 @@ class SloppyTree(dict):
                 yield path
 
 
-class SloppyException(LookupError):
-    def __init__(self, message, original_exception=None):
-        super().__init__(message)
-        self.original_exception = original_exception
-
-    def raise_original(self):
-        """Re-raise the orignal exception if it exists"""
-        if self.original_exception:
-            raise self.original_exception
-        else:
-            raise self 
